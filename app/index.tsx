e540ef990ed24e9abd2e1706d8047e9a3bc2a053
AlexanderJumbo <geossephyjumboalava@gmail.com>
2025-10-25 20:54:38 -0500

    Configuraciones

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { ThemedText } from "@/components/themed-text";
+import React from "react";
+import { View } from "react-native";
+
+const PushApp = () => {
+  return (
+    <View>
+      <ThemedText>Hola mundo</ThemedText>
+    </View>
+  );
+};
+
+export default PushApp;
